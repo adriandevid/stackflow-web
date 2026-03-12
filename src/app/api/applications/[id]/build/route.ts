78012af4ec5dbd6eaba08612b3544c0f61d50261
AdrianDevid <adriandevid36@gmail.com>
2026-03-12 16:54:57 -0300

    adicionado ajuste de aplicação de configuração

--- src/app/api/applications/[id]/build/route.ts
+++ src/app/api/applications/[id]/build/route.ts
@@ -7,22 +7,21 @@ async function GET(request: NextRequest, { params }: { params: Promise<{ id: num
 
     const rows = localdatabase.prepare(`select * from application where id = ${id}`).all() as { name: string }[];
     
-    const executeCommand = new Promise((resolve, reject) => {
-        exec("kubectl ", (error, stdout, stderr) => {
+    const executeCommand = new Promise<string>((resolve, reject) => {
+        exec(`kubectl apply -f ./configuration/applications/${rows[0].name}/deployment.yml`, (error, stdout, stderr) => {
             if (error) {
                 reject(`exec error: ${error}`);
                 return;
             }
-
-            const disks = stdout.split(/(?=^[A-Z]:)/gm);
-            const selectedDisck = disks.filter(x => x.includes("C:"))[0].replaceAll("\\r", "").replaceAll("\\n", "").split(/(\d\S*)/gm);
-
-            resolve({
-                freeSpace: parseInt(selectedDisck[1].replaceAll(" ", "")),
-                size: parseInt(selectedDisck[3].replaceAll(" ", ""))
-            })
+            resolve(`${stdout} - ${stderr}`);
         })
     });
 
-    return NextResponse.json({}, { status: 200 })
+    const result = await executeCommand;
+
+    return NextResponse.json({
+        result: result
+    }, { status: 200 })
 }
+
+export { GET }
