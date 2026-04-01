9e516a1cd9487298f6f34ac4c0cc92277eeb15a7
AdrianDevid <adriandevid36@gmail.com>
2026-04-01 16:39:51 -0300

    ajuste na configuração do pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,8 @@
 module.exports = {
   apps : [{
     name   : "pedreiro web",
-    script : "./server.ts",
-    watch: false,
-    interpreter: "node",
-    env_production: {
-       NODE_ENV: "production"
-    },
-    env_development: {
-       NODE_ENV: "development"
-    }
+    script : "npm",
+    args   : "start",
+    watch: false
   }]
 };
